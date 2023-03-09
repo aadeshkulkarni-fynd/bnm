d1293a512f8eedcb69db097a1e16ffd8e5aff0d1
Aadesh Kulkarni <aadeshkulkarni@gofynd.com>
2023-03-09 17:27:57 +0530

    initial commit

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const API_1 = "https://5kxkhs7v2h.execute-api.ap-south-1.amazonaws.com/dev/qsemb
 const API_2 = "https://5kxkhs7v2h.execute-api.ap-south-1.amazonaws.com/dev/qsembeddemo-bnm?key=BNM-DEMO-QS-FEB-2023&mode=getAuthor";
 
 function makeAPIcall(URL) {
-  fetch(URL)
+  fetch(URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
@@ -22,6 +28,6 @@ function showAdmin() {
   makeAPIcall(API_2);
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    showViewer();
+document.addEventListener("DOMContentLoaded", function () {
+  showViewer();
 });
