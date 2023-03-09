698f8deff08a6ed7ae2e6bd365c72410cd3e826a
Aadesh Kulkarni <aadeshkulkarni@gofynd.com>
2023-03-09 19:23:27 +0530

    using axios

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 const ifDashboard = document.getElementById("ifDashboard");
+const URL = "https://5kxkhs7v2h.execute-api.ap-south-1.amazonaws.com/dev/qsembeddemo-bnm?key=BNM-DEMO-QS-FEB-2023&mode=getAuthor";
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-const API_1 = "https://5kxkhs7v2h.execute-api.ap-south-1.amazonaws.com/dev/qsembeddemo-bnm?key=BNM-DEMO-QS-FEB-2023&mode=getAuthor";
-const API_2 = "https://5kxkhs7v2h.execute-api.ap-south-1.amazonaws.com/dev/qsembeddemo-bnm?key=BNM-DEMO-QS-FEB-2023&mode=getAuthor";
 
-function makeAPIcall(URL) {
-
+function showViewer() {
   axios.get(URL)
     .then((response) => {
       console.log(response);
-      // response.json()
+      ifDashboard.src = response.data.DashboardEmbedUrl;
     })
-    // .then((json) => {
-    //   console.log(json);
-    //   ifDashboard.src = json.DashboardEmbedUrl;
-    // });
-}
-
-function showViewer() {
-  console.log("Called 1");
-  makeAPIcall(API_1);
 }
 
 function showAdmin() {
-  console.log("Called 2");
-  makeAPIcall(API_2);
+  axios.get(URL)
+    .then((response) => {
+      console.log(response);
+      ifDashboard.src = response.data.QEmbedUrl;
+    })
 }
 
 document.addEventListener("DOMContentLoaded", function () {
