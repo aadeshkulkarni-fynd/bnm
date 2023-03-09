442b3ac3b4135f3802602b8269c10255b884078e
Aadesh Kulkarni <aadeshkulkarni@gofynd.com>
2023-03-09 17:11:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const ifDashboard=document.getElementById("ifDashboard");
+
+const API_1='https://5kxkhs7v2h.execute-api.ap-south-1.amazonaws.com/dev/qsembeddemo-bnm?key=BNM-DEMO-QS-FEB-2023&mode=getAuthor'
+
+// fetch(API_1)
+//   .then(response => response.json())
+//   .then(json => console.log(json))
