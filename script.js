7f377c55f45d27575568c5a5ce66831731b01943
Aadesh Kulkarni <aadeshkulkarni@gofynd.com>
2023-03-09 17:16:16 +0530

    basic refactoring

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const ifDashboard=document.getElementById("ifDashboard");
+const ifDashboard = document.getElementById("ifDashboard");
 
-const API_1='https://5kxkhs7v2h.execute-api.ap-south-1.amazonaws.com/dev/qsembeddemo-bnm?key=BNM-DEMO-QS-FEB-2023&mode=getAuthor'
+const API_1 = "https://5kxkhs7v2h.execute-api.ap-south-1.amazonaws.com/dev/qsembeddemo-bnm?key=BNM-DEMO-QS-FEB-2023&mode=getAuthor";
+const API_2 = "https://5kxkhs7v2h.execute-api.ap-south-1.amazonaws.com/dev/qsembeddemo-bnm?key=BNM-DEMO-QS-FEB-2023&mode=getAuthor";
 
-// fetch(API_1)
-//   .then(response => response.json())
-//   .then(json => console.log(json))
+function makeAPIcall(URL) {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      ifDashboard.src = json.DashboardEmbedUrl;
+    });
+}
+
+function showViewer() {
+  console.log("Called 1");
+  makeAPIcall(API_1);
+}
+
+function showAdmin() {
+  console.log("Called 2");
+  makeAPIcall(API_2);
+}
